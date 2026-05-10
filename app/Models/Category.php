82211818a7669ff8beadd335357199bfabda21b3
Selvina <selvina2005@example.com>
2026-05-10 21:59:08 +0700

    Update Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
  protected $fillable = ['name', 'slug'];
- // Menandakan atribut: 1 Kategori dapat memiliki banyak list Event
+
  public function events()
- {
- return $this->hasMany(Event::class);
- }
+    {
+    return $this->hasMany(Event::class);
+    }
 }
