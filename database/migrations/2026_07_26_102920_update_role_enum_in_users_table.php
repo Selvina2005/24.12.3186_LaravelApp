83235858687265dc8419edbf5a8031168e3c51ae
Selvina <selvina2005@example.com>
2026-07-26 17:32:33 +0700

    Update user role enum for organization system

---
+++ database/migrations/2026_07_26_102920_update_role_enum_in_users_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement("
+            ALTER TABLE users 
+            MODIFY role ENUM('superadmin','organizer','user') DEFAULT 'user'
+        ");
+    }
+
+    public function down(): void
+    {
+        DB::statement("
+            ALTER TABLE users 
+            MODIFY role ENUM('admin','user') DEFAULT 'user'
+        ");
+    }
+};
