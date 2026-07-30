0d43268556fbf877a5ea514d7e15e91cb688abbd
Selvina <selvina2005@example.com>
2026-07-30 19:05:41 +0700

    FP BUSINESS DIGITAL

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
+
 class TransactionController extends Controller
 {
     public function index()
     {
-        // Mengambil transaksi terbaru dengan pembatasan 20 baris/halaman
-        $transactions = Transaction::with('event')->latest()->paginate(20);
-        return view('admin.transactions.index',
-        compact('transactions'));
+        $user = auth()->user();
+
+        if ($user->role == 'superadmin') {
+
+            // Super Admin melihat semua transaksi
+            $transactions = Transaction::with(['event.organization'])
+                ->latest()
+                ->paginate(20);
+
+        } else {
+
+            // Organizer hanya melihat transaksi organisasinya
+            $transactions = Transaction::with(['event.organization'])
+                ->whereHas('event', function ($query) use ($user) {
+                    $query->where('organization_id', $user->organization_id);
+                })
+                ->latest()
+                ->paginate(20);
+        }
+
+        return view('admin.transactions.index', compact('transactions'));
     }
 }
